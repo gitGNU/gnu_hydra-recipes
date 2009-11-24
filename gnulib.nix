281a760b28be5563551ee7a91128cf048caa5133
Rob Vermaas <rob.vermaas@gmail.com>
2009-11-24 09:27:28 +0000

    git-svn-id: https://svn.nixos.org/repos/nix/hydra-config/gnu/trunk@18585 70bd8c7a-acb8-0310-9f0d-9cc1c95dcdbb

--- gnulib.nix
+++ gnulib.nix
@@ -1,7 +1,7 @@
 pkgs:
 pkgs.fetchgit {
   url = git://git.savannah.gnu.org/gnulib.git;
-  rev = "6038ee4b827caaf05fa37dbb2304fedb9d0cd6c7";
-  sha256 = "8d13d4dcd6cde4ca5f91fa6aff90205ded691cfaa30d436348389a2280018b11";
+  rev = "0fe2cbf6bb3dcf60a7c4004c332f9ef6ea855290";
+  sha256 = "e401eab0dbff4493aeec75ad2af0582e646389d6454cc8be875469d0fe9aa955";
 }
 
