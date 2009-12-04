b110eecce5021b28deeac79fc8b5854712767d7d
Ludovic Courtès <ludo@gnu.org>
2009-12-04 14:37:46 +0000

    autoconf: Add a `manual' job.
    git-svn-id: https://svn.nixos.org/repos/nix/hydra-config/gnu/trunk@18803 70bd8c7a-acb8-0310-9f0d-9cc1c95dcdbb

--- autoconf/release.nix
+++ autoconf/release.nix
@@ -2,10 +2,7 @@
 let
   pkgs = import nixpkgs {};
 
-  buildInputsFrom = pkgs: with pkgs; [
-    perl 
-    m4
-  ];
+  buildInputsFrom = pkgs: with pkgs; [ perl m4 ];
 
   jobs = rec {
 
@@ -18,7 +15,7 @@ let
       pkgs.releaseTools.makeSourceTarball {
         name = "autoconf-tarball";
         src = autoconfSrc;
-        preConfigurePhases = "preAutoconfPhase autoconfPhase"; 
+        preConfigurePhases = "preAutoconfPhase autoconfPhase";
         preAutoconfPhase = ''
           echo -n "$(git describe)" > .tarball-version
         '';
@@ -43,6 +40,25 @@ let
         buildInputs = buildInputsFrom pkgs;
       };
 
+    manual =
+      { tarball ? jobs.tarball {}
+      }:
+
+      releaseTools.nixBuild {
+        name = "autoconf-manual";
+        src = tarball;
+        buildInputs = [ pkgs.texinfo pkgs.texLive ];
+
+        buildPhase = "make html pdf";
+        doCheck = false;
+        installPhase =
+          '' make -C doc install-html install-pdf
+
+             ensureDir "$out/nix-support"
+             echo "doc manual $out/share/doc/autoconf/autoconf.html index.html" >> "$out/nix-support/hydra-build-products"
+             echo "doc-pdf manual $out/share/doc/autoconf/autoconf.pdf" >> "$out/nix-support/hydra-build-products"
+          '';
+      };
   };
 
 in jobs
