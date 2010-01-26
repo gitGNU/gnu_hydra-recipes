118095c7da3d27fca334b5a9ab274a7f94ba57d1
Ludovic Courtès <ludo@gnu.org>
2010-01-26 16:49:05 +0000

    tar: Remove patch that no longer applies.
    git-svn-id: https://svn.nixos.org/repos/nix/hydra-config/gnu/trunk@19681 70bd8c7a-acb8-0310-9f0d-9cc1c95dcdbb

--- tar/release.nix
+++ tar/release.nix
@@ -30,9 +30,6 @@ let
           ./bootstrap --gnulib-srcdir=../gnulib --paxutils-srcdir=../paxutils --skip-po --copy
         '';
 
-        # apply patch until it get's accepted upstream (http://www.mail-archive.com/bug-tar@gnu.org/msg02390.html)
-        patches = [./tar-remote-shell-correction.patch]; 
-
         buildInputs = [
           git
           gettext
