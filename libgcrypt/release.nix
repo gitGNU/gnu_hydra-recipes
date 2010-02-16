483b31694140a6fc36fa951767f60bf298534d80
Ludovic Courtès <ludo@gnu.org>
2010-02-16 12:55:54 +0000

    libgcrypt: Add `meta'.
    git-svn-id: https://svn.nixos.org/repos/nix/hydra-config/gnu/trunk@20049 70bd8c7a-acb8-0310-9f0d-9cc1c95dcdbb

--- libgcrypt/release.nix
+++ libgcrypt/release.nix
@@ -1,6 +1,25 @@
 { nixpkgs ? ../../nixpkgs }:
 
 let
+  meta = {
+    description = "GNU Libgcrypt, a general-pupose cryptographic library";
+
+    longDescription = ''
+      GNU Libgcrypt is a general purpose cryptographic library based on
+      the code from GnuPG.  It provides functions for all
+      cryptographic building blocks: symmetric ciphers, hash
+      algorithms, MACs, public key algorithms, large integer
+      functions, random numbers and a lot of supporting functions.
+    '';
+
+    license = "LGPLv2+";
+
+    homepage = http://gnupg.org/;
+
+    # Those who will receive email notifications.
+    maintainers = [ "hydra-logs@gnupg.org" ];
+  };
+
   pkgs = import nixpkgs {};
 
   inherit (pkgs) releaseTools;
@@ -25,6 +44,8 @@ let
                libtool_1_5 # the repo contains Libtool 1.5's `ltmain.sh', etc.
 	       subversion texinfo transfig ghostscript
 	      ]);
+
+        inherit meta;
       };
 
     build =
@@ -40,6 +61,7 @@ let
           src = tarball;
           buildInputs = (buildInputsFrom pkgs);
           propagatedBuildInputs = [ libgpgerror ];
+          inherit meta;
         };
 
     coverage =
@@ -51,6 +73,7 @@ let
 	name = "libgcrypt-coverage";
 	src = tarball;
 	buildInputs = (buildInputsFrom (import nixpkgs {})) ++ [ libgpgerror ];
+        inherit meta;
       };
 
     manual =
@@ -74,6 +97,7 @@ let
              echo "doc manual $out/share/doc/libgcrypt/gcrypt.html index.html" >> "$out/nix-support/hydra-build-products"
              echo "doc-pdf manual $out/share/doc/libgcrypt/gcrypt.pdf" >> "$out/nix-support/hydra-build-products"
           '';
+        inherit meta;
       };
   };
 
