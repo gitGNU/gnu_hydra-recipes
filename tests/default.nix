9aff5d4769bdccd1b05bc1410360c09ba1f60903
Ludovic Courtès <ludo@gnu.org>
2010-03-14 15:06:18 +0000

    tests: Try to use the latest GNU packages.
    git-svn-id: https://svn.nixos.org/repos/nix/hydra-config/gnu/trunk@20615 70bd8c7a-acb8-0310-9f0d-9cc1c95dcdbb

--- tests/default.nix
+++ tests/default.nix
@@ -5,9 +5,24 @@
 , gnuOverrides
 }:
 
-# FIXME: Somehow pass `gnuOverrides' to the testing framework.
 with import "${nixos}/lib/testing.nix" { inherit nixpkgs services system; };
+with import "${nixos}/lib/build-vms.nix" { inherit nixpkgs services system; };
 
-{
-  version = apply (import ./version.nix);
-}
+let
+  call = f: f { inherit nixpkgs system; pkgs = gnuOverrides pkgs; };
+
+  apply = testFun: complete (call testFun);
+
+  complete = t: t // rec {
+    nodes =
+      if t ? nodes then t.nodes else
+      if t ? machine then { machine = t.machine; }
+      else { };
+    vms = buildVirtualNetwork { inherit nodes; };
+    test = runTests vms t.testScript;
+    report = makeReport test;
+  };
+in
+  {
+    version = apply (import ./version.nix);
+  }
