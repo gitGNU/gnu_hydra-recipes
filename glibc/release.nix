eb8370d53f90dcf5a18030063771ab182e44fcb1
Ludovic Courtès <ludo@gnu.org>
2010-03-15 00:26:15 +0000

    Add tentative glibc expression.
    git-svn-id: https://svn.nixos.org/repos/nix/hydra-config/gnu/trunk@20624 70bd8c7a-acb8-0310-9f0d-9cc1c95dcdbb

---
+++ glibc/release.nix
@@ -0,0 +1,96 @@
+{ nixpkgs ? ../../nixpkgs }:
+
+let
+  pkgs = import nixpkgs {};
+
+  inherit (pkgs) releaseTools;
+
+  buildInputsFrom = pkgs: with pkgs; [ gettext texinfo ];
+
+  # Build out-of-tree.
+  preConfigure =
+    ''
+       mkdir ../build
+       cd ../build
+
+       configureScript="../$sourceRoot/configure"
+    '';
+
+  jobs = rec {
+
+    tarball =
+      { glibcSrc ? { outPath = /data/src/glibc; } }:
+
+      releaseTools.sourceTarball {
+	name = "glibc-tarball";
+	src = glibcSrc;
+
+        patches =
+          (map (x: "${nixpkgs}/pkgs/development/libraries/glibc-2.11/${x}")
+               [ "locale-override.patch"       # NixOS-specific
+                 "rpcgen-path.patch"           # submit upstream?
+                 "stack-protector-link.patch"  # submit upstream?
+               ])
+          ++ [ ./ignore-git-diff.patch ];      # allow for "make dist"
+
+
+        # The repository contains Autoconf-generated files & co.
+        autoconfPhase = "true";
+
+        # Remove absolute paths from `configure' & co.; build out-of-tree.
+        preConfigure =
+          ''
+             export PWD_P=$(type -tP pwd)
+             for i in configure io/ftwtest-sh; do
+                 sed -i "$i" -e "s^/bin/pwd^$PWD_P^g"
+             done
+
+             ${preConfigure}
+          '';
+
+        postConfigure = "set -x"; # XXX
+
+        buildInputs = (buildInputsFrom pkgs) ++ [ pkgs.git pkgs.xz ];
+
+        # Jump back to where the tarballs are and copy them from there.
+        dontCopyDist = true;
+        postDist =
+          ''
+             cd "../$sourceRoot"
+             ensureDir "$out/tarballs"
+             mv -v glibc-*.tar.{bz2,gz,xz} "$out/tarballs"
+          '';
+      };
+
+    build =
+      { tarball ? jobs.tarball {}
+      , system ? "x86_64-linux"
+      }:
+
+      let pkgs = import nixpkgs { inherit system; };
+      in
+        pkgs.releaseTools.nixBuild {
+          name = "glibc";
+          src = tarball;
+          configureFlags = "--with-headers=${pkgs.kernelHeaders}/include";
+          buildInputs = buildInputsFrom pkgs;
+          inherit preConfigure;
+        };
+
+    coverage =
+      { tarball ? jobs.tarball {}
+      }:
+
+      let pkgs = import nixpkgs { system = "x86_64-linux"; };
+      in
+        releaseTools.coverageAnalysis {
+          name = "glibc-coverage";
+          src = tarball;
+          configureFlags = "--with-headers=${pkgs.kernelHeaders}/include";
+          buildInputs = buildInputsFrom pkgs;
+          inherit preConfigure;
+        };
+
+  };
+
+in jobs
