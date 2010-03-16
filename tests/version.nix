bc052506d6106198b6d050a5e4caf1685da5df7a
Ludovic Courtès <ludo@gnu.org>
2010-03-16 14:40:35 +0000

    tests: Fix global environment; make stdout visible in test logs.
    git-svn-id: https://svn.nixos.org/repos/nix/hydra-config/gnu/trunk@20658 70bd8c7a-acb8-0310-9f0d-9cc1c95dcdbb

--- tests/version.nix
+++ tests/version.nix
@@ -1,14 +1,20 @@
 { pkgs, ... }:
 
 {
-  machine = { config, pkgs, ... }: { };
+  machine = { config, pkgs, ... }: {
+    # Extra packages wanted in the global environment.
+    environment.systemPackages =
+      [ pkgs.cpio pkgs.guile_1_9 pkgs.inetutils ];
+  };
 
   testScript =
     ''
-      $machine->mustSucceed("touch --version");
-      $machine->mustSucceed("tar --version");
-      $machine->mustSucceed("cpio --version");
-      $machine->mustSucceed("guile --version");
-      $machine->mustSucceed("guile -c '(format #t \"hello, world!~%\")'");
+      $machine->mustSucceed("ldd --version >&2");
+      $machine->mustSucceed("touch --version >&2");
+      $machine->mustSucceed("tar --version >&2");
+      $machine->mustSucceed("cpio --version >&2");
+      $machine->mustSucceed("ifconfig --version >&2");
+      $machine->mustSucceed("guile --version >&2");
+      $machine->mustSucceed("guile -c '(format #t \"hello, world!~%\")' >&2");
     '';
 }
