4b6f87245d697c8548d1b26a0e85637af9c7610a
Rob Vermaas <rob.vermaas@gmail.com>
2010-03-17 09:51:23 +0000

    added gzip/release.nix
    git-svn-id: https://svn.nixos.org/repos/nix/hydra-config/gnu/trunk@20683 70bd8c7a-acb8-0310-9f0d-9cc1c95dcdbb

---
+++ gzip/release.nix
@@ -0,0 +1,61 @@
+{ nixpkgs ? ../../nixpkgs 
+, gzip ? { outPath = ../../gzip; }
+, gnulib ? {outPath = ../../gnulib;}
+}:
+
+let
+  pkgs = import nixpkgs {};
+
+  jobs = with pkgs; rec {
+
+    tarball =
+      pkgs.releaseTools.makeSourceTarball {
+	name = "gzip-tarball";
+	src = gzip;
+
+        autoconfPhase = ''
+          mkdir -p ../gnulib
+          cp -Rv ${gnulib}/* ../gnulib
+          chmod -R 755 ../gnulib
+#          export ACLOCAL_PATH=$ACLOCAL_PATH:../gnulib/m4
+
+          ./bootstrap --gnulib-srcdir=../gnulib --skip-po --copy
+        '';
+
+	buildInputs = [
+          automake111x
+          texinfo
+          gettext
+          git 
+          wget
+          perl
+          rsync
+          xz
+	];
+      };
+
+    build =
+      { system ? "x86_64-linux"
+      }:
+
+      releaseTools.nixBuild {
+	name = "gzip" ;
+	src = tarball;
+	buildInputs = [];
+      };
+
+    coverage =
+      { tarball ? jobs.tarball {}
+      }:
+
+      with pkgs;
+
+      releaseTools.coverageAnalysis {
+        name = "gzip-coverage";
+        src = tarball;
+        buildInputs = [];
+      };
+
+  };
+
+in jobs
