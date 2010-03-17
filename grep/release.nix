0a7466cc92ff24ab2595e1c1c47f945a6ea767b0
Rob Vermaas <rob.vermaas@gmail.com>
2010-03-17 11:46:13 +0000

    added grep/release.nix
    git-svn-id: https://svn.nixos.org/repos/nix/hydra-config/gnu/trunk@20693 70bd8c7a-acb8-0310-9f0d-9cc1c95dcdbb

---
+++ grep/release.nix
@@ -0,0 +1,57 @@
+{ nixpkgs ? ../../nixpkgs 
+, grep ? { outPath = ../../gzip; }
+, gnulib ? {outPath = ../../gnulib;}
+}:
+
+let
+  pkgs = import nixpkgs {};
+
+  jobs = rec {
+
+    tarball =
+      pkgs.releaseTools.makeSourceTarball {
+	name = "grep-tarball";
+	src = grep;
+
+        autoconfPhase = ''
+          mkdir -p ../gnulib
+          cp -Rv ${gnulib}/* ../gnulib
+          chmod -R 755 ../gnulib
+
+          ./bootstrap --gnulib-srcdir=../gnulib --skip-po --copy
+        '';
+
+	buildInputs = with pkgs; [
+          automake111x
+          texinfo
+          gettext
+          git 
+          perl
+          rsync
+          xz
+	];
+      };
+
+    build =
+      { system ? "x86_64-linux"
+      }:
+      let pkgs = import nixpkgs {inherit system;};
+      in with pkgs;
+      releaseTools.nixBuild {
+	name = "grep" ;
+	src = tarball;
+	buildInputs = [];
+      };
+
+    coverage =
+      with pkgs;
+
+      releaseTools.coverageAnalysis {
+        name = "grep-coverage";
+        src = tarball;
+        buildInputs = [];
+      };
+
+  };
+
+in jobs
