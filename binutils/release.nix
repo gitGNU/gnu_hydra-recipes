8063314fd13328657097de52de15bdcc2f15c974
Ludovic Courtès <ludo@gnu.org>
2010-03-18 15:26:56 +0000

    Add GNU Binutils.
    git-svn-id: https://svn.nixos.org/repos/nix/hydra-config/gnu/trunk@20743 70bd8c7a-acb8-0310-9f0d-9cc1c95dcdbb

---
+++ binutils/release.nix
@@ -0,0 +1,72 @@
+{ nixpkgs ? ../../nixpkgs
+, binutilsSrc ? { outPath = /data/src/binutils; }
+}:
+
+let
+  pkgs = import nixpkgs {};
+
+  inherit (pkgs) releaseTools;
+
+  checkPhase = "make -k check";
+  failureHook =
+    '' echo "build failed, dumping log files..."
+       for log in $(find -name \*.log)
+       do
+         echo
+         echo "--- $log"
+         cat "$log"
+       done
+    '';
+
+  jobs = rec {
+
+    tarball =
+      releaseTools.sourceTarball {
+        name = "binutils-tarball";
+        src = binutilsSrc;
+        autoconfPhase = "true";
+        buildInputs = with pkgs;
+          [ texinfo gettext flex2535 bison ];
+
+        distPhase =
+          ''
+             make -f src-release "binutils.tar.bz2"
+             ensureDir "$out/tarballs"
+             mv -v binutils*.bz2 "$out/tarballs"
+          '';
+      };
+
+    build =
+      { system ? "x86_64-linux"
+      , tarball ? jobs.tarball
+      }:
+
+      let pkgs = import nixpkgs { inherit system; };
+      in
+        pkgs.releaseTools.nixBuild {
+          name = "binutils";
+          src = tarball;
+
+          # FIXME: Looks like some GNU ld tests want libdwarf.
+          buildInputs = [ pkgs.dejagnu pkgs.zlib ];
+          inherit checkPhase failureHook;
+        };
+
+    buildGold =
+      { system ? "x86_64-linux"
+      , tarball ? jobs.tarball
+      }:
+
+      let pkgs = import nixpkgs { inherit system; };
+      in
+        pkgs.releaseTools.nixBuild {
+          name = "binutils-gold";
+          src = tarball;
+          configureFlags = "--enable-gold";
+          buildInputs = [ pkgs.dejagnu pkgs.zlib ];
+          inherit checkPhase failureHook;
+        };
+  };
+
+in
+  jobs
