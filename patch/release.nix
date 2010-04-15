754e6fb2384c66c1e66a71eddd923d014671251e
Ludovic Courtès <ludo@gnu.org>
2010-04-15 08:46:58 +0000

    Add GNU Patch.
    git-svn-id: https://svn.nixos.org/repos/nix/hydra-config/gnu/trunk@21085 70bd8c7a-acb8-0310-9f0d-9cc1c95dcdbb

---
+++ patch/release.nix
@@ -0,0 +1,46 @@
+{ nixpkgs ? { outPath = ../../nixpkgs; }
+, gnulib ? { outPath = ../../gnulib; }
+, patchSrc ? { outPath = ../../patch; } }:
+
+let
+  pkgs = import nixpkgs {};
+
+  jobs = {
+    tarball =
+      with pkgs;
+      releaseTools.sourceTarball {
+        name = "patch";
+        src = patchSrc;
+        buildInputs = [ xz gettext texinfo automake111x ];
+        autoconfPhase =
+          '' cp -Rv "${gnulib}/"* gnulib/
+             chmod -R 755 gnulib
+
+             ./bootstrap --gnulib-srcdir=gnulib --skip-po --copy
+          '';
+      };
+
+    build =
+      { system ? builtins.currentSystem
+      , tarball ? jobs.tarball }:
+
+      let pkgs = import nixpkgs { inherit system; };
+      in
+        pkgs.releaseTools.nixBuild {
+          name = "patch";
+          src = tarball;
+        };
+
+    coverage =
+      { system ? builtins.currentSystem
+      , tarball ? jobs.tarball }:
+
+      let pkgs = import nixpkgs { inherit system; };
+      in
+        pkgs.releaseTools.coverageAnalysis {
+          name = "patch";
+          src = tarball;
+        };
+  };
+in
+  jobs
