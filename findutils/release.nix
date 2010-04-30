1a08a929b7fc2a05b9a9b12bee5a41947d9d4ecb
Ludovic Courtès <ludo@gnu.org>
2010-04-30 16:47:42 +0000

    Add GNU Findutils.
    git-svn-id: https://svn.nixos.org/repos/nix/hydra-config/gnu/trunk@21462 70bd8c7a-acb8-0310-9f0d-9cc1c95dcdbb

---
+++ findutils/release.nix
@@ -0,0 +1,73 @@
+{ nixpkgs ? { outPath = ../../nixpkgs; }
+, gnulib ? { outPath = ../../gnulib; }
+, findutilsSrc ? { outPath = ../../findutils; } }:
+
+let
+  pkgs = import nixpkgs {};
+
+  meta = {
+    description = "GNU Findutils, a program to find files";
+
+    longDescription =
+      '' The GNU Find Utilities are the basic directory searching utilities
+         of the GNU operating system. These programs are typically used in
+         conjunction with other programs to provide modular and powerful
+         directory search and file locating capabilities to other commands.
+      '';
+
+    homepage = http://savannah.gnu.org/projects/findutils;
+
+    license = "GPLv3+";
+
+    maintainers =
+     [ "James Youngman <jay@gnu.org>"
+       pkgs.stdenv.lib.maintainers.ludo
+     ];
+  };
+
+  jobs = {
+    tarball =
+      with pkgs;
+      releaseTools.sourceTarball {
+	name = "findutils-tarball";
+	src = findutilsSrc;
+	buildInputs =
+          [ automake111x gettext gperf bison groff git
+            texinfo xz
+            cvs # for `autopoint'
+          ];
+	autoconfPhase =
+          # `gnulib-tool' wants write access to the Gnulib directory, e.g.,
+          # to create `./build-aux/arg-nonnull.h.tmp'.  Thus we have to copy
+          # the whole Gnulib tree in a writable place.
+	  '' cp -rv "${gnulib}" ../gnulib
+             chmod -R u+w ../gnulib
+             sh ./import-gnulib.sh -d ../gnulib
+	  '';
+	inherit meta;
+      };
+
+    build =
+      { system ? builtins.currentSystem
+      , tarball ? jobs.tarball }:
+
+      let pkgs = import nixpkgs { inherit system; };
+      in
+	pkgs.releaseTools.nixBuild {
+	  name = "findutils";
+	  src = tarball;
+	  inherit meta;
+	};
+
+    coverage =
+      { tarball ? jobs.tarball }:
+
+      let pkgs = import nixpkgs {};
+      in
+	pkgs.releaseTools.coverageAnalysis {
+	  name = "findutils-coverage";
+	  src = tarball;
+	};
+  };
+in
+  jobs
