f087498d027847e6406a6ae0b6f8c7d877a43695
Ludovic Courtès <ludo@gnu.org>
2010-05-30 23:10:43 +0000

    guile: Temporarily disable error-on-warning.
    git-svn-id: https://svn.nixos.org/repos/nix/hydra-config/gnu/trunk@22063 70bd8c7a-acb8-0310-9f0d-9cc1c95dcdbb

--- guile/release.nix
+++ guile/release.nix
@@ -47,7 +47,10 @@ let
   defaultConfigureFlags = pkgs:
      with pkgs;
 
-     ((stdenv.lib.optional stdenv.isLinux "--enable-error-on-warning")
+     # FIXME: Temporarily commented out because of libunistring issues.
+     # http://lists.gnu.org/archive/html/bug-gnulib/2010-05/msg00226.html
+     (#(stdenv.lib.optional stdenv.isLinux "--enable-error-on-warning")
+      []
 
      # The `--with' flags below aren't strictly needed, except on Cygwin
      # where the added `-L' linker flags help Libtool find the dlls, which in
