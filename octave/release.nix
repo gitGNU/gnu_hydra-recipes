ed1ea25fb257e20c81600994dcc69b8491ef394c
Rob Vermaas <rob.vermaas@gmail.com>
2010-08-03 08:34:31 +0000

    git-svn-id: https://svn.nixos.org/repos/nix/hydra-config/gnu/trunk@22917 70bd8c7a-acb8-0310-9f0d-9cc1c95dcdbb

--- octave/release.nix
+++ octave/release.nix
@@ -23,7 +23,7 @@ let
 
   pkgs = import nixpkgs {};
   
-  buildInputsFrom = pkgs: with pkgs; [gfortran readline ncurses perl qhull blas liblapack pcre imagemagick gnuplot fftw zlib ghostscript transfig xfig pstoedit hdf5 texinfo qrupdate suitesparse curl fltk11] ;
+  buildInputsFrom = pkgs: with pkgs; [gfortran readline ncurses perl qhull blas liblapack pcre imagemagick gnuplot fftw zlib ghostscript transfig xfig pstoedit hdf5 texinfo qrupdate suitesparse curl fltk11 texLive] ;
   
   jobs = rec {
 
@@ -49,7 +49,7 @@ let
         '';
 
         buildInputs = [
-          flex2535 git gperf bison texLive automake111x] ++ buildInputsFrom pkgs ;
+          flex2535 git gperf bison automake111x] ++ buildInputsFrom pkgs ;
       };
 
     build =
