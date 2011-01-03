ef3c9e43205c2861b7caa1f0e6243937be393b1b
Rob Vermaas <rob.vermaas@gmail.com>
2011-01-03 20:34:27 +0000

    gnu-hydra: add rcs
    git-svn-id: https://svn.nixos.org/repos/nix/hydra-config/gnu/trunk@25372 70bd8c7a-acb8-0310-9f0d-9cc1c95dcdbb

---
+++ rcs/release.nix
@@ -0,0 +1,91 @@
+/* Continuous integration of GNU with Hydra/Nix.
+   Copyright (C) 2010  Ludovic Courtès <ludo@gnu.org>
+   Copyright (C) 2010  Rob Vermaas <rob.vermaas@gmail.com>
+
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.  */
+
+{ nixpkgs ? ../../nixpkgs 
+}:
+
+let
+  pkgs = import nixpkgs {};
+
+  meta = {
+    homepage = http://www.gnu.org/software/rcs/;
+    description = "The Revision Control System (RCS) manages multiple revisions of files.";
+
+    longDescription = ''
+      The Revision Control System (RCS) manages multiple revisions of files. 
+      RCS automates the storing, retrieval, logging, identification, and merging 
+      of revisions. RCS is useful for text that is revised frequently, including 
+      source code, programs, documentation, graphics, papers, and form letters.
+    '';
+
+    license = "GPLv3+";
+
+    # Those who will receive email notifications.
+    maintainers = [
+      "Rob Vermaas <rob.vermaas@gmail.com>"
+    ];
+  };
+
+  configureFlags = "RCS_PRETEST=acknowledged";
+
+  jobs = rec {
+
+    tarball = 
+      { rcs ? { outPath = ../../rcs; }
+      , gnulib ? {outPath = ../../gnulib;}
+      }:
+      pkgs.releaseTools.makeSourceTarball {
+	name = "rcs-tarball";
+	src = rcs;
+        inherit meta configureFlags; 
+        autoconfPhase = ''
+          cp -Rv ${gnulib} ../gnulib
+          chmod -R a+rwx ../gnulib
+          export PATH=$PATH:../gnulib
+          sh autogen.sh
+        '';
+        buildInputs = with pkgs; [ automake111x autoconf ed texinfo emacs];
+      };
+
+    build =
+      { system ? "x86_64-linux"
+      , tarball ? jobs.tarball {}
+      }:
+      let pkgs = import nixpkgs {inherit system;};
+      in with pkgs;
+      releaseTools.nixBuild {
+        name = "rcs" ;
+        src = tarball;
+        inherit meta configureFlags;
+        buildInputs = with pkgs; [ed];
+      } ;
+
+    coverage =
+      { tarball ? jobs.tarball {}
+      }:
+      with pkgs;
+
+      releaseTools.coverageAnalysis {
+        name = "rcs-coverage";
+        src = tarball;
+        inherit meta configureFlags;
+        buildInputs = with pkgs; [ed];
+      };
+
+  };
+
+in jobs
