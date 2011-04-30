4d2d9cbf170129723edfdd2fa54eb70505a8bba8
Ludovic Courtès <ludo@gnu.org>
2011-04-30 14:45:41 +0000

    Add GNU MPC.
    git-svn-id: https://svn.nixos.org/repos/nix/hydra-config/gnu/trunk@27061 70bd8c7a-acb8-0310-9f0d-9cc1c95dcdbb

---
+++ mpc/release.nix
@@ -0,0 +1,63 @@
+/* Continuous integration of GNU with Hydra/Nix.
+   Copyright (C) 2011  Ludovic Courtès <ludo@gnu.org>
+
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.  */
+
+{ nixpkgs ? { outPath = ../../nixpkgs; }
+, mpcSrc ? { outPath = ../../mpc; }
+, gmp ? (import nixpkgs {}).gmp      # native GMP build
+, gmp_xgnu ? null                     # cross-GNU GMP build
+, mpfr ? (import nixpkgs {}).mpfr    # native MPFR build
+, mpfr_xgnu ? null                    # cross-GNU MPFR build
+}:
+
+let
+  meta = {
+    description = "GNU MPC, a library for multi-precision complex numbers";
+
+    longDescription =
+      '' GNU MPC is a C library for the arithmetic of complex numbers with
+         arbitrarily high precision and correct rounding of the result. It is
+         built upon and follows the same principles as GNU MPFR.
+      '';
+
+    homepage = http://mpc.multiprecision.org/;
+
+    license = "LGPLv3+";
+
+    maintainers =
+     [ (import nixpkgs {}).stdenv.lib.maintainers.ludo
+     ];
+  };
+in
+  import ../gnu-jobs.nix {
+    name = "mpc";
+    src  = mpcSrc;
+    inherit nixpkgs meta;
+    useLatestGnulib = false;
+    enableGnuCrossBuild = true;
+
+    customEnv = {
+
+      tarball = pkgs: {
+	buildInputs = [ gmp mpfr ]
+          ++ (with pkgs; [ texinfo automake111x ]);
+        autoconfPhase = "autoreconf -vfi";
+      };
+
+      build = pkgs: { buildInputs = [ gmp mpfr ]; };
+      coverage = pkgs: { buildInputs = [ gmp mpfr ]; };
+      xbuild_gnu = pkgs: { buildInputs = [ gmp_xgnu mpfr_xgnu ]; };
+    };
+  }
