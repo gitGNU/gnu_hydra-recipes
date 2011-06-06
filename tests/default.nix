83c1e49c235e9540b8d3708f36d0226f1e2800ac
Rob Vermaas <rob.vermaas@gmail.com>
2011-06-06 19:18:04 +0000

    adapt code to new nixos/lib, should refactor, but lazy for now
    git-svn-id: https://svn.nixos.org/repos/nix/hydra-config/gnu/trunk@27355 70bd8c7a-acb8-0310-9f0d-9cc1c95dcdbb

--- tests/default.nix
+++ tests/default.nix
@@ -22,34 +22,61 @@
 
 with import "${nixos}/lib/testing.nix" { inherit nixpkgs system; };
 with import "${nixos}/lib/build-vms.nix" { inherit nixpkgs system; };
-
+with pkgs;
 let
+  gnuify = name: configFunction:
+              builtins.trace "node `${name}'"
+              (args: (configFunction args) // gnuConfigOptions);
+
   call = f: f { inherit nixpkgs system pkgs; };
 
   apply = testFun: complete (call testFun);
 
   complete = t: t // rec {
-    nodes =
+    nodes = buildVirtualNetwork ( pkgs.lib.mapAttrs gnuify (
       if t ? nodes then t.nodes else
       if t ? machine then { machine = t.machine; }
-      else { };
-    vms = buildVirtualNetwork {
-      # Build a network of nodes that use `gnuConfigOptions', i.e., the
-      # latest GNU packages and a GNU configuration.
-      nodes =
-        let gnuify = name: configFunction:
-              builtins.trace "node `${name}'"
-              (args: (configFunction args) // gnuConfigOptions);
-        in
-          pkgs.lib.mapAttrs gnuify nodes;
-    };
+      else { } ));
 
-    test = (runTests vms t.testScript) // {
-      meta.schedulingPriority = "5";
-    };
+    testScript =
+      # Call the test script with the computed nodes.
+      if builtins.isFunction t.testScript
+      then t.testScript { inherit nodes; }
+      else t.testScript;
+
+    vlans = map (m: m.config.virtualisation.vlans) (lib.attrValues nodes);
+
+    vms = map (m: m.config.system.build.vm) (lib.attrValues nodes);
+
+    # Generate onvenience wrappers for running the test driver
+    # interactively with the specified network, and for starting the
+    # VMs from the command line.
+    driver = runCommand "nixos-test-driver"
+      { buildInputs = [ makeWrapper];
+        inherit testScript;
+      }
+      ''
+        mkdir -p $out/bin
+        echo "$testScript" > $out/test-script
+        ln -s ${testDriver}/bin/nixos-test-driver $out/bin/
+        vms="$(for i in ${toString vms}; do echo $i/bin/run-*-vm; done)"
+        wrapProgram $out/bin/nixos-test-driver \
+          --add-flags "$vms" \
+          --run "testScript=\"\$(cat $out/test-script)\"" \
+          --set testScript '"$testScript"' \
+          --set VLANS '"${toString vlans}"'
+        ln -s ${testDriver}/bin/nixos-test-driver $out/bin/nixos-run-vms
+        wrapProgram $out/bin/nixos-run-vms \
+          --add-flags "$vms" \
+          --set tests '"startAll; sleep 1e9;"' \
+          --set VLANS '"${toString vlans}"'
+      ''; # "
+
+    test = runTests driver;
 
     report = makeReport test;
   };
+
 in
   {
     version = apply (import ./version.nix);
