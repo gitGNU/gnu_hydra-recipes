7b63c4d08678b18254df2ef6294bfaf47e3e94f7
Ludovic Courtès <ludo@gnu.org>
2011-10-30 01:40:02 +0200

    gnutls: Switch to Gettext 0.18.

--- gnutls/release.nix
+++ gnutls/release.nix
@@ -46,7 +46,7 @@ let
   inherit (pkgs) releaseTools;
 
   buildInputsFrom = pkgs: with pkgs;
-    [ xz zlib lzo gettext_0_17 pkgconfig perl nettle]
+    [ xz zlib lzo gettext pkgconfig perl nettle]
     ++ stdenv.lib.optional stdenv.isLinux valgrind
     ++ stdenv.lib.optional (stdenv.isDarwin || stdenv.isBSD) libiconv;
 
