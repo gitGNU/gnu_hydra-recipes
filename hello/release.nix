c303ce484327016fc936e5b3f33981af44cd80d3
Rob Vermaas <rob.vermaas@gmail.com>
2012-04-13 16:26:54 +0200

    Add GNU hello jobset.

---
+++ hello/release.nix
@@ -0,0 +1,53 @@
+/* Continuous integration of GNU with Hydra/Nix.
+   Copyright (C) 2012  Rob Vermaas <rob.vermaas@gmail.com>
+
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.  */
+
+{ nixpkgs ? <nixpkgs>
+, hello ? { outPath = <hello>; }
+}:
+
+let
+  meta = {
+    description = "A program that produces a familiar, friendly greeting";
+    longDescription = ''
+      GNU Hello is a program that prints "Hello, world!" when you run it.
+      It is fully customizable.
+    '';
+    homepage = http://www.gnu.org/software/hello/manual/;
+
+    license = "GPLv3+";
+
+    # Those who will receive email notifications.
+    maintainers = [
+      "Reuben Thomas <rrt@sc3d.org>"
+    ];
+  };
+in
+  import ../gnu-jobs.nix {
+    name = "hello";
+    src  = hello;
+    inherit nixpkgs meta;
+    enableGnuCrossBuild = true;
+
+    customEnv = {
+      tarball = pkgs: {
+        autoconfPhase = ''
+          ./autogen.sh
+        '';
+        dontBuild = false;
+        buildInputs = with pkgs; [gettext help2man texinfo];
+      };
+    };
+  }
