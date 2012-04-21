1eef81d92bce12d5c76541edfff43d36bd58de64
Ludovic Courtès <ludo@gnu.org>
2012-04-21 23:08:49 +0200

    GMP, MPFR: Use angle brackets for inputs in the search path.

--- gmp/release.nix
+++ gmp/release.nix
@@ -14,8 +14,8 @@
    You should have received a copy of the GNU General Public License
    along with this program.  If not, see <http://www.gnu.org/licenses/>.  */
 
-{ nixpkgs ? { outPath = ../../nixpkgs; }
-, gmpSrc ? { outPath = ../../gmp; } }:
+{ nixpkgs ? <nixpkgs>
+, gmpSrc ? <gmp> }:
 
 let
   meta = {
--- mpfr/release.nix
+++ mpfr/release.nix
@@ -14,8 +14,8 @@
    You should have received a copy of the GNU General Public License
    along with this program.  If not, see <http://www.gnu.org/licenses/>.  */
 
-{ nixpkgs ? { outPath = ../../nixpkgs; }
-, mpfrSrc ? { outPath = ../../mpfr; }
+{ nixpkgs ? <nixpkgs>
+, mpfrSrc ? <mpfr>
 , gmp ? (import nixpkgs {}).gmp      # native GMP build
 , gmp_xgnu ? null                     # cross-GNU GMP build
 }:
