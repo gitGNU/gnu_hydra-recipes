697c855e7c720f1dc5da70436310134c640b2157
Ludovic Courtès <ludo@gnu.org>
2012-06-11 15:01:57 +0200

    guile: Add `xbuild_mips_linux_gnu'.

--- guile/release.nix
+++ guile/release.nix
@@ -393,6 +393,17 @@ let
       makeCrossBuild "x86_64-linux" crossSystems.mipsel_nanonote_linux_gnu
         (xpkgs: [ ]);
 
+    xbuild_mips_linux_gnu =
+      # Cross build to `mips-linux-gnu' (big endian).
+      makeCrossBuild "x86_64-linux"
+        (crossSystems.mipsel_nanonote_linux_gnu
+         //
+         {
+           bigEndian = true;
+           config = "mips-unknown-linux-gnu";
+           float = "hard";
+         })
+        (xpkgs: [ ]);
   }
 
   //
