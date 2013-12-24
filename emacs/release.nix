ff98a5551f40408a0500b2927d7cb46100f902ae
Glenn Morris <rgm@gnu.org>
2013-12-24 11:49:17 -0800

    emacs: (tarball) Also build the info files Need to do this since we stopped bootstrapping.

--- emacs/release.nix
+++ emacs/release.nix
@@ -87,7 +87,7 @@ in
 
         configurePhase = ":";
 	distPhase = ''
-	  make
+	  make info all
 	  ./make-dist --tar --tests --no-update
 	  ensureDir $out/tarballs
 	  cp -pvd *.tar.gz $out/tarballs
