13e0d2b4a8ef018188fda107dbd525a6cf7b7492
Ludovic Courtès <ludo@gnu.org>
2014-11-29 21:28:55 +0100

    coreutils: Add x86_64-darwin, as requested by Pádraig Brady.

--- coreutils/release.nix
+++ coreutils/release.nix
@@ -40,7 +40,7 @@ let
   };
 
   # Systems we want to build for.
-  systems = [ "x86_64-linux" "i686-linux" ];
+  systems = [ "x86_64-linux" "i686-linux" "x86_64-darwin" ];
 
   pkgs = import nixpkgs {};
   crossSystems = (import ../cross-systems.nix) { inherit pkgs; };
