4ab474ba84bfaa39b836271c32108ebd4bc54dd0
Glenn Morris <rgm@gnu.org>
2015-05-11 17:28:01 -0400

    emacs: Add git to tarball inputs (for make ChangeLog)

--- emacs/release.nix
+++ emacs/release.nix
@@ -72,7 +72,7 @@ in
       tarball = pkgs: {
 	# FIXME Move --enable-check-lisp-object-type here from coverage?
 	configureFlags = "";
-	buildInputs = with pkgs; [ texinfo ncurses pkgconfig perl ];
+	buildInputs = with pkgs; [ texinfo ncurses pkgconfig perl git ];
 
         # patches = [ ./bug11251.patch ];
         # enableParallelBuilding = true;
